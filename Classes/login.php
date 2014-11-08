4c677ddd10e6ea91c04bdab6712930c18dd12ac7
David <=>
2014-11-08 17:49:36 -0600

    Began writing the other controlling functions.

--- Classes/login.php
+++ Classes/login.php
@@ -1,30 +1,30 @@
 <?php
 /**
  * Created by IntelliJ IDEA.
- * User: david
+ * User: David
  * Date: 11/8/15
  * Time: 4:28 PM
  */
-//$m = new MongoClient()h;
-// The current database
-//$db = $m->selectDB("mydb");
 
+// include the user class
+require "user.php";
 
     /*
-     * This handles the user log in.
-     */
+     * This handles the user log in. */
     function authenticateUser() {
         // Check if the email or username and password combo is correct.
-        $enteredName = $_POST('username');
+        $enteredName = $_POST('email');
         $enteredPass = $_POST('password');
         if(empty($enteredName) ||empty($enteredPass) ) {
             // Should probably send an error.
-            return false;
+            alert()
+
+            return False;
         }
-        if(!(strpos($enteredName,"@") === False)) {
-            // user has entered an email
+        if((strpos($enteredName,"@") === False)) {
+            // user has not entered a password.
             // error handling.
-            return false;
+            return False;
         }
         return userLogin($enteredName,$enteredPass);
     }
@@ -32,20 +32,37 @@
     /*
      * Adds a user to the database
      */
-    function signup($enteredName,$enteredEmail,$enteredPass1,$enteredPass2) {
+    function signup() {
         // ensure the password and password confirmation are equal to each other.
+        $enteredName = $_POST('email');
+        $enteredPass1 = $_POST('password1');
+        $enteredPass2 = $_POST('password2');
+        // check that passwords are the same.
+        if(!($enteredPass1 === $enteredPass2)) {
+            // Should show an error.
+            return False;
+        }
+        if(!(strpos($enteredName,'@') === False)) {
+            // Should tell user that email was wrong
+            // Should also check that it's a valid domain.
+            return False;
+        }
+        // create the user.
+        return true;
     }
 
     /*
      * Starts the session for the user
      */
     function startSession() {
+        // Not sure how do do this
     }
 
     /*
      * Logs a user out of their session.
      */
     function signout() {
+        // Also not sure how to do this.
     }
 
 ?>
